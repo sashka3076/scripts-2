33e61598c1b2e77b05988c102289f4843f364fdf
solo <50051085+solo10010@users.noreply.github.com>
2022-02-16 12:31:42 +0600

    Update bitrix-correction.sh

--- Bitrix/bitrix-correction.sh
+++ Bitrix/bitrix-correction.sh
@@ -2,4 +2,14 @@
 
 DEBUG_STD="&> bitrix-correction.log"
 
-yum install vim mc -y
+echo "Устанавливаем vim mc"
+
+yum install vim mc -y $DEBUG_STD
+
+echo "Начинаем править cron"
+
+touch /etc/cron.d/bx_$HOSTNAME $DEBUG_STD
+echo "*/1 * * * * /usr/bin/php -f /home/bitrix/www/bitrix/modules/main/tools/cron_events.php" > /etc/cron.d/bx_$HOSTNAME $DEBUG_STD
+cat /etc/cron.d/bx_$HOSTNAME $DEBUG_STD
+
+echo "Начинаем править почту"
