83e775c008b262724500b655431975bc1fb8beee
solo10010 <tohac2011@gmail.com>
2022-04-26 10:43:27 +0600

    script

--- check-iscsi/check-iscsi.sh
+++ check-iscsi/check-iscsi.sh
@@ -150,10 +150,8 @@ function Check_Size_Dir(){
     total_size=$(echo $dir_size | awk '{print $2}')
 
     # получаем криты именно по процентам ели больще 90% занято то ошибка grep -o "^[0-9][0-9]\?"
-    #pri=$(echo $procent_use | grep -o '^[0-9][0-9]\?')
-    percent=90
-    echo $pri
-    if [ $(echo $procent_use | grep -o '^[0-9][0-9]\?') -gt 90 ]; then
+
+    if [ $(echo $procent_use | grep -o '^[0-9][0-9]\?') -gt 90 ]; then # если 90 больше то
         echo "$(date +'%Y.%m.%d.%k') На диске заканчивается место $procent_use"
         echo "$(date +'%Y.%m.%d.%k') На диске заканчивается место $procent_use" >> $ERROR_LOG
     fi
